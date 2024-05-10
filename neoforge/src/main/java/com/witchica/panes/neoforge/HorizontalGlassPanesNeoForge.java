82b952470e73347cdbb8f86bef2a611a330f04c9
Jess <jessicastrongg@gmail.com>
2024-05-11 00:50:59 +0100

    1.20.4

---
+++ neoforge/src/main/java/com/witchica/panes/neoforge/HorizontalGlassPanesNeoForge.java
@@ -0,0 +1,11 @@
+package com.witchica.panes.neoforge;
+
+import com.witchica.panes.common.HorizontalGlassPanes;
+import net.neoforged.fml.common.Mod;
+
+@Mod(HorizontalGlassPanes.MOD_ID)
+public class HorizontalGlassPanesNeoForge {
+    public HorizontalGlassPanesNeoForge() {
+        HorizontalGlassPanes.onInitialize();
+    }
+}
