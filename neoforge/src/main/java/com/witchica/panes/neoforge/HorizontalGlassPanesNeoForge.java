5ff06f9bf9f8017b5d718acd33155baa248b9430
Jess <jessicastrongg@gmail.com>
2024-05-11 01:40:31 +0100

    1.20.6 port, apart from Architectury being broken for  NeoForge

--- neoforge/src/main/java/com/witchica/panes/neoforge/HorizontalGlassPanesNeoForge.java
+++ neoforge/src/main/java/com/witchica/panes/neoforge/HorizontalGlassPanesNeoForge.java
@@ -1,11 +1,12 @@
 package com.witchica.panes.neoforge;
 
 import com.witchica.panes.common.HorizontalGlassPanes;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(HorizontalGlassPanes.MOD_ID)
 public class HorizontalGlassPanesNeoForge {
-    public HorizontalGlassPanesNeoForge() {
+    public HorizontalGlassPanesNeoForge(IEventBus bus) {
         HorizontalGlassPanes.onInitialize();
     }
 }
